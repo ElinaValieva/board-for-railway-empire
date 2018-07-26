529e2157d7f8a871936dca2e3cc6e125bd69e019
ElinaValieva <veaufa@mail.ru>
2018-07-26 22:34:35 +0300

    feat: add msg listener

--- src/main/com/elina/railwayApp/ScheduleBean.java
+++ src/main/com/elina/railwayApp/ScheduleBean.java
@@ -2,21 +2,34 @@ package elina.railwayApp;
 
 import elina.railwayApp.model.Schedule;
 import elina.railwayApp.model.TimeSchedule;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 @ManagedBean
 @SessionScoped
 public class ScheduleBean {
     private List<Schedule> schedules;
+
+    @Getter
+    @Setter
     private List<TimeSchedule> schedulesDeparture;
+
+    @Getter
+    @Setter
     private List<TimeSchedule> schedulesArrival;
+
+
     private List<String> stations;
     private String selectedItem = "Saint Petersburg";
+    private Listener listener = new Listener();
 
     public String getSelectedItem() {
         return selectedItem;
@@ -34,25 +47,15 @@ public class ScheduleBean {
     }
 
     @PostConstruct
-    private void init() throws IOException {
+    private void init() throws IOException, TimeoutException {
+        listener.start();
         schedules = Loader.getSchedules();
         schedulesDeparture = Converter.conventDeparture(selectedItem, schedules);
         schedulesArrival = Converter.convertArrival(selectedItem, schedules);
     }
 
-    public List<TimeSchedule> getSchedulesDeparture() {
-        return schedulesDeparture;
-    }
-
-    public void setSchedulesDeparture(List<TimeSchedule> schedulesDeparture) {
-        this.schedulesDeparture = schedulesDeparture;
-    }
-
-    public List<TimeSchedule> getSchedulesArrival() {
-        return schedulesArrival;
-    }
-
-    public void setSchedulesArrival(List<TimeSchedule> schedulesArrival) {
-        this.schedulesArrival = schedulesArrival;
+    @PreDestroy
+    private void destroy() throws IOException, TimeoutException {
+        listener.stop();
     }
 }
