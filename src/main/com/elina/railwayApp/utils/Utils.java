c1a6be266f89ddf14637996b65a817dc0f8dce4a
ElinaValieva <veaufa@mail.ru>
2018-08-01 12:47:11 +0300

    fix: url for schedule by id

--- src/main/com/elina/railwayApp/utils/Utils.java
+++ src/main/com/elina/railwayApp/utils/Utils.java
@@ -8,7 +8,7 @@ public class Utils {
 
     public static final String URL_SCHEDULES = "http://localhost:8000/schedule/today";
     public static final String URL_STATIONS = "http://localhost:8000/station/auto/stations";
-    public static final String URL_SCHEDULE_BY_ID = "http://localhost:8000/schedule/";
+    public static final String URL_SCHEDULE_BY_ID = "http://localhost:8000/schedule/get/";
 
 
     public static String parseToTime(String date) throws ParseException {
